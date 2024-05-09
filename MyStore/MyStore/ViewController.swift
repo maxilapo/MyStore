4dcbd84fd61bc408f4207badc5bc5421d46f5252
Maxime Lapointe <maxlapo@gmail.com>
2024-05-09 15:13:59 -0400

    first commit

---
+++ MyStore/MyStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStore
+//
+//  Created by Maxime Lapointe on 2024-05-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
